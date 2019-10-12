05bec08f547b9269da172a566b353fc97c04068e
MoonSold <55290172+MoonSold@users.noreply.github.com>
2019-10-12 23:31:31 +0300

    Add files via upload

--- Socket.py
+++ Socket.py
@@ -1,2 +1,17 @@
-# HalloWorld
-print
+from socket import * 
+
+
+
+
+
+MY_SOCKET = socket(AF_INET,SOCK_STREAM) 
+MY_SOCKET.bind(("localhost",5000))
+MY_SOCKET.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+MY_SOCKET.listen() 
+
+while True: 
+			
+
+	client_socket,adress = MY_SOCKET.accept() 
+	client_socket.sendall(("HTTP/1.1 200 OK\n\n" + "<h1>Привет мир</h1>").encode()) 
+	client_socket.close()  
