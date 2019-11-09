4d06e7de15d315fe4591dc146bd70a60cb102c30
MoonSold <55290172+MoonSold@users.noreply.github.com>
2019-11-09 21:28:28 +0300

    Add files via upload

--- BASE.py
+++ BASE.py
@@ -1,16 +1,40 @@
 from sqlite3 import *  
 
-MY_BASE = connect("ALL_BASE_PASSWORD.db") 
+MY_BASE = connect("ALL_USERS_PASSWORD.db") 
 
-CURSOR = MY_BASE.cursor()  
+CURSOR = MY_BASE.cursor()   
+
+LIST = []
+
+L = None
+
+while L != "":
+	
+	NAMES = input("Введите имя, пароль и мэйл(через запятую): ") 
+	print("Если хотите выйти нажмиите ENTER при пустом вводе")    
+	PERSON = NAMES.split(",")  
+	LIST.append(PERSON) 
+	
+	if NAMES == "":  
+		
+		L = ""
+
+R = LIST.pop()
+R = None
+
+print(LIST)
 
 
-CURSOR.execute("""CREATE TABLE base(SNP TEXT,PASSWORD TEXT,MAIL TEXT)""") 
 
-CURSOR.execute("""INSERT INTO base (SNP,PASSWORD,MAIL) VALUES ("Seliverstov Nickita Denisovich","s78945","LP45@mail.ru" )""")
-CURSOR.execute("""INSERT INTO base (SNP,PASSWORD,MAIL) VALUES ("Perov Michail Urievich","qwerty123","POI7@mail.ru")""")
-CURSOR.execute("""INSERT INTO base (SNP,PASSWORD,MAIL) VALUES ("Anton Riabov Andreevich","s745454","ART@mail.ru")""")
 
+CURSOR.execute("""CREATE TABLE users(SNP TEXT,PASSWORD TEXT,MAIL TEXT)""") 
+
+
+for PER in LIST:
+	CURSOR.execute("""INSERT INTO users (SNP,PASSWORD,MAIL) VALUES (?,?,? )""",PER)
+
+ 
+print("Введено неправильно")
 
 
 MY_BASE.commit() 
--- Socket.py
+++ Socket.py
@@ -1,39 +1,39 @@
 from socket import * 
 from sqlite3 import *
 
-MY_BASE = connect("ALL_BASE_PASSWORD.db") 
+MY_BASE = connect("ALL_USERS_PASSWORD.db") 
 CURSOR = MY_BASE.cursor() 
 
 
 LIST = []
 
-NAME1 = CURSOR.execute("""SELECT * FROM base """)
+NAME1 = CURSOR.execute("""SELECT * FROM users""")
 
 for i in NAME1: 
 	LIST.append(i) 
 
 
 
-FIRST,SECOND,THIRD = LIST
+
+TABLE = "<table>\n" + "<style type=\"text/css\">\nTABLE {\nwidth: 100%;\nborder-collapse: collapse;\n}\nTD, TH {\npadding: 3px;\nborder: 1px solid black;\n}\nTH {\nbackground: Yellow;\n}\n</style>\n"+ "<tr><th>FIO</th><th>PASSWORD</th><th>MAIL</th></tr>\n"
+
+for NPE in LIST: 
+	
+	FIO,MAIL,PASSWORD = NPE 
+
+	STR = "<tr><td>" + str(FIO) + "</td><td>" + str(MAIL) + "</td><td>" + str(PASSWORD) + "</td></tr>\n"
+
+	TABLE = TABLE + STR
 
 
 
-NAME1,PASSWORD1,MAIL1 = FIRST
-NAME2,PASSWORD2,MAIL2 = SECOND
-NAME3,PASSWORD3,MAIL3 = THIRD
-
-
-TABLE = "<table>\n" + "<style type=\"text/css\">\nTABLE {\nwidth: 100%;\nborder-collapse: collapse;\n}\nTD, TH {\npadding: 3px;\nborder: 1px solid black;\n}\nTH {\nbackground: Yellow;\n}\n</style>\n"+ "<tr><th>FIO</th><th>PASSWORD</th><th>MAIL</th></tr>""<tr><td>" + str(NAME1) + "</td><td>" + str(PASSWORD1) + "</td><td>" + str(MAIL1) + "</td></tr>\n" + "<tr><td>" + str(NAME2) + "</td><td>" + str(PASSWORD2) + "</td><td>" + str(MAIL2) + "</td></tr>\n" + "<tr><td>" + str(NAME3) + "</td><td>" + str(PASSWORD3) + "</td><td>" + str(MAIL3) + "</td></tr>\n" + "</table>"
-
-
+TABLE = TABLE + "</table>"
 
 
 
 
 
 MY_BASE.close()
-
-
 MY_SOCKET = socket(AF_INET,SOCK_STREAM) 
 MY_SOCKET.bind(("localhost",5000))
 MY_SOCKET.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
@@ -53,7 +53,9 @@ while True:
 			client_socket.sendall(("HTTP/1.1 200 OK\n\n" + "<h1>HALLO WORLD</h1>").encode()) 
 	
 		client_socket.close()  
+	
 	except: 
+		
 		pass
 
 
